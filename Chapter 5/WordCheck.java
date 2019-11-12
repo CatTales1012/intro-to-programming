279964a0d8d13a9511d4399ac0617d6394d01769
CatTales <student@HSiMac4-7.local>
2019-11-12 13:05:59 -0700

    Added indents and fixed the name

--- Chapter 5/WordCheck.java
+++ Chapter 5/WordCheck.java
@@ -15,18 +15,19 @@ class WordCheck {
 
 		s = s.toLowerCase();
 
-	for (int i = 0; i < s.length(); ++i){
-		char ch = s.charAt(i);
-			
-		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			for (int i = 0; i < s.length(); ++i){
+				char ch = s.charAt(i);
+					
+				if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 				vowels++;
-		}
-		else{
-			consonants++;
-		}
+				}
+				
+				else{
+				consonants++;
 	}
-		
-System.out.println(s + " has " + vowels + " vowels.");
-System.out.print(s + " has " + consonants + " consonants. ");
+}
+				
+		System.out.println(s + " has " + vowels + " vowels.");
+		System.out.print(s + " has " + consonants + " consonants. ");
 	}
 }
