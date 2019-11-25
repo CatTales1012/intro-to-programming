917cd730337ca1b9ef0042fd81477e2c256a4723
CatTales1012 <43213984+CatTales1012@users.noreply.github.com>
2019-11-25 13:36:08 -0700

    Added Java Docs

--- Chapter 6/Matrix-1.java
+++ Chapter 6/Matrix-1.java
@@ -2,11 +2,21 @@
 Author: Cat Smith
 Assignment 6-17
 Date: Nov 15
+Revised: Nov 25
 */
-
+package jdoc;
 import java.util.Scanner;
 
 class Matrix {
+	/**
+	 * <h1>Matrix JavaDocs</h1>
+	 * <h2>Created November 15, 2019 <br> Revisited November 25, 2019</h2>
+	 * @author Cat Smith
+	 * 
+	 * <p>This is a program that generates a matrix given a number
+	 * (prints an N by N grid, with randomly generated numbers).</p>
+	 * @version 1.0.5
+	 */
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
 		
@@ -17,7 +27,10 @@ class Matrix {
 		printMatrix(n);
 			
 	}
-	
+	/**
+	 * This is the method for generating the matrix given 'n'.
+	 * @param n the number inputed to print out the grid.
+	 */
 	public static void printMatrix(int n){
 		for(int i = 0; i < n; i++){
 			// Start a row
