f725addb5c9c9a99bf9f1d8d5a079316b1b654cb
CatTales1012 <43213984+CatTales1012@users.noreply.github.com>
2019-12-02 11:18:00 -0700

    Added JavaDocs

--- Chapter 6/ReversePalindrome.java
+++ Chapter 6/ReversePalindrome.java
@@ -3,10 +3,19 @@ Author: Cat Smith
 Assignment: 6-3, palindrome/reverse methods
 date: Nov 15, 2019
 */
-
+package d3;
 import java.util.Scanner;
 
-class ReversePalindrome{
+public class ReversePalindrome{
+	/**
+	 * <h1 style="color:green;">Reverse Palindrome JavaDocs</h1>
+	 * <h2 style="color:blue;">Created Nov 15<br> Revisited Dec 2</h2>
+	 * @author Cat Smith
+	 * @version 1.5
+	 * <p>This is a program to reverse a number and 
+	 * also check if it is a Palindrome</p>
+	 * @param args is the main method.
+	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
@@ -25,6 +34,11 @@ class ReversePalindrome{
 			System.out.print(number + " is not a palindrome.");
 		}
 	}
+	/**
+	 * 
+	 * @param number is the number that is inputted by a user.
+	 * @return the number inputted reversed.
+	 */
 	public static int reverse(int number){
 		int reverse = 0;
 		
@@ -35,6 +49,11 @@ class ReversePalindrome{
 		}
 		return reverse;
 	}
+	/** 
+	 * 
+	 * @param number is the number inputted by the user.
+	 * @return whether the value is a palindrome or not.
+	 */
 	public static boolean isPalindrome(int number){
 		reverse(number);
 		
