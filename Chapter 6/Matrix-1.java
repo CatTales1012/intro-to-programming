e45742874fa5cd354e9c2fe30ddbac0a79e23c6a
CatTales1012 <43213984+CatTales1012@users.noreply.github.com>
2019-12-02 11:04:34 -0700

    JavaDocs now better functional

--- Chapter 6/Matrix-1.java
+++ Chapter 6/Matrix-1.java
@@ -4,15 +4,15 @@ Assignment 6-17
 Date: Nov 15
 Revised: Nov 25
 */
-package jdoc;
+package d17;
 import java.util.Scanner;
 
-class Matrix {
+public class Matrix {
 	/**
 	 * <h1>Matrix JavaDocs</h1>
 	 * <h2>Created November 15, 2019 <br> Revisited November 25, 2019</h2>
 	 * @author Cat Smith
-	 * 
+	 * @param args part of the main method
 	 * <p>This is a program that generates a matrix given a number
 	 * (prints an N by N grid, with randomly generated numbers).</p>
 	 * @version 1.0.5
