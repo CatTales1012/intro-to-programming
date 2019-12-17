e024e71e526c2764dd451c6a29d0d0898a180e7f
CatTales <student@HSiMac4-7.local>
2019-12-17 12:41:36 -0700

    Game made workable, finally.

--- Chapter 8/TicTacToe.java
+++ Chapter 8/TicTacToe.java
@@ -1,7 +1,7 @@
 /*
 Author: Cat Smith
 Assignment: 8-9, Tic Tac Toe
-Date: Dec 16
+Date: Dec 17
 */
 import java.util.Scanner;
 class TicTacToe {
@@ -12,6 +12,10 @@ class TicTacToe {
 			{"[]", "[]", "[]"},
 			{"[]", "[]", "[]"}
 		};
+		//[] == available spaces 
+		int counter = 0;
+		String winner = null;
+		
 		for (int row = 0; row < 3; row++){
 			for(int col = 0; col < 3; col++){
 				System.out.print(board[row][col] + " ");
@@ -19,11 +23,19 @@ class TicTacToe {
 			System.out.println();
 		}
 		
-		for (int i = 0; i < 9; i++){
+		while (winner == null) {
 			System.out.print("Player X: Enter 0, 1, or 2 for row: ");
 			int xRow = input.nextInt();
 			System.out.print("Player X: Enter 0, 1, or 2 for column: ");
 			int xCol = input.nextInt();
+			
+			while (!board[xRow][xCol].equals("[]")) {
+				System.out.println("That location is taken. Try another.");
+				System.out.print("Player X: Enter 0, 1, or 2 for row: ");
+				xRow = input.nextInt();
+				System.out.print("Player X: Enter 0, 1, or 2 for column: ");
+				xCol = input.nextInt();
+			}
 			
 			board[xRow][xCol] = "X";
 			
@@ -34,10 +46,67 @@ class TicTacToe {
 				System.out.println();
 			}
 			
+			if ((board[0][0]).equals("X") && (board[0][1]).equals("X") && (board[0][2]).equals("X")){
+				winner = "X";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[1][0]).equals("X") && (board[1][1]).equals("X") && (board[1][2]).equals("X")){
+				winner = "X";
+				System.out.print("Player " + winner + " Wins!");
+			} 
+			else if ((board[2][0]).equals("X") && (board[2][1]).equals("X") && (board[2][2]).equals("X")){
+				winner = "X";
+				System.out.print("Player " + winner + " Wins!");
+			}
+			else if ((board[0][0]).equals("X") && (board[1][1]).equals("X") && (board[2][2]).equals("X")){ 
+				winner = "X";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[0][2]).equals("X") && (board[1][1]).equals("X") && (board[2][0]).equals("X")){
+				winner = "X";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[0][0]).equals("O") && (board[0][1]).equals("O") && (board[0][2]).equals("O")){
+				winner = "O";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[1][0]).equals("O") && (board[1][1]).equals("O") && (board[1][2]).equals("O")){
+				winner = "O";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[2][0]).equals("O") && (board[2][1]).equals("O") && (board[2][2]).equals("O")){
+				winner = "O";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if ((board[0][0]).equals("O") && (board[1][1]).equals("O") && (board[2][2]).equals("O")){
+				winner = "O";
+				System.out.print("Player " + winner + " Wins!");
+			} 
+			else if ((board[0][2]).equals("O") && (board[1][1]).equals("O") && (board[2][0]).equals("O")){
+				winner = "O";
+				System.out.print("Player " + winner + " Wins!");
+			}  
+			else if(counter > 9) {
+				winner = "No one";
+				System.out.println("No one won this round!");
+			}
+			
+			if (winner != null) {
+				System.exit(0);
+			}
+			
 			System.out.print("Player O: Enter 0, 1, or 2 for row: ");
 			int oRow = input.nextInt();
 			System.out.print("Player O: Enter 0, 1, or 2 for column: ");
 			int oCol = input.nextInt();
+			
+			while (!board[oRow][oCol].equals("[]")) {
+				System.out.println("That location is taken. Try another.");
+				System.out.print("Player O: Enter 0, 1, or 2 for row: ");
+				oRow = input.nextInt();
+				System.out.print("Player O: Enter 0, 1, or 2 for column: ");
+				oCol = input.nextInt();
+			}
 			
 			board[oRow][oCol] = "O";
 			
@@ -48,22 +117,8 @@ class TicTacToe {
 				System.out.println();
 			}
 			
-			String winner = null;
-			switch (winner) {
-				case (board[0][0] && board [0][1] && board[0][2]).equals("X"): winner = "X"; break;
-				case (board[1][0] && board [1][1] && board[1][2]).equals("X"): winner = "X"; break;
-				case (board[2][0] && board [2][1] && board[2][2]).equals("X"): winner = "X"; break;
-				case (board[0][0] && board [1][1] && board[2][2]).equals("X"): winner = "X"; break;
-				case (board[0][2] && board [0][1] && board[2][0]).equals("X"): winner = "X"; break;
-				case (board[0][0] && board [0][1] && board[0][2]).equals("O"): winner = "O"; break;
-				case (board[1][0] && board [1][1] && board[1][2]).equals("O"): winner = "O"; break;
-				case (board[2][0] && board [2][1] && board[2][2]).equals("O"): winner = "O"; break;
-				case (board[0][0] && board [1][1] && board[2][2]).equals("O"): winner = "O"; break;
-				case (board[0][2] && board [0][1] && board[2][0]).equals("O"): winner = "O"; break;
-				default: System.out.println("Nobody won..");
-			}	
+			counter += 2;
 			
-			System.out.print("Player " + winner + "Wins!");
 		}
 	}
 }
